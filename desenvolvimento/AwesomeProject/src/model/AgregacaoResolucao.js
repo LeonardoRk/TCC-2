ee55e03bac9d7f51c21f5da6aca397df244a366b
LeonardoRk <leodegolim@yahoo.com.br>
2019-03-26 01:22:19 -0300

    mundo de resolução sem questões repetidas

--- desenvolvimento/AwesomeProject/src/model/AgregacaoResolucao.js
+++ desenvolvimento/AwesomeProject/src/model/AgregacaoResolucao.js
@@ -3,13 +3,31 @@ import ResolucaoFactory from '../model/ResolucaoFactory'
 
 
 const QUANTIDADE_PERGUNTAS = 10;
+
+verificaExistencia = (array, numFigura) =>{
+    let existe = false;
+    for(let j = 0 ; j < array.length; j++){
+        if(numFigura == array[j]){
+            existe = true;
+        }
+    }
+    return existe;
+}
+
 export default class AgregacaoResolucao extends Component{
     constructor(nomeFase){
         super();
         let objetoResolucao = ResolucaoFactory.constroiResolucao(nomeFase);
         let todasPerguntas = [];
+        let numFigura = null;
+        let jaExiste = false;
         for(let i = 0 ; i < QUANTIDADE_PERGUNTAS; i++){
-            let numFigura = objetoResolucao.figuraAleatoria();
+            numFigura = objetoResolucao.figuraAleatoria();
+            jaExiste = verificaExistencia(todasPerguntas, numFigura);
+            while(jaExiste){
+                numFigura = objetoResolucao.figuraAleatoria();
+                jaExiste = verificaExistencia(todasPerguntas, numFigura);
+            }
             todasPerguntas.push(numFigura);
         }
         console.log("Todas as perguntas selecionadas : " + todasPerguntas);
